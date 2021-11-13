2536bf30835fd6911e18e04654f1ffe4a388e18c
Edwin Alfaro <edwinalfaro9908@gmail.com>
2021-11-13 13:15:07 -0500

    async requests

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
 export default store;
